de4a44102ad124174de590aad1ed7c0afd9d2722
Harish <144200799+harish2196@users.noreply.github.com>
2024-04-22 15:19:58 +0530

    Interface

--- StudentDetails.java
+++ StudentDetails.java
@@ -1,86 +1,50 @@
-package com.chainsys.abs;
-abstract class Student {
+package com.chainsys.methodoverriding;
+import java.util.Scanner;
+
+interface Students {
+	abstract public  void study();
+	abstract public void takeExam();
+}
+
+class HighSchoolStudent implements Students {
 	String name;
 	int age;
+	String studentId;
 
-	public Student(String name, int age) {
+	public void displayInformation() {
+		System.out.println("Name: " + name + "\nAge: " + age + "\nStudent ID: " + studentId);
+	}
+
+	public HighSchoolStudent(String name, int age, String studentId) {
+
 		this.name = name;
 		this.age = age;
-	}
-
-	public abstract void getDetails();
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public abstract void displayDetails();
-}
-
-class HighSchoolStudent extends Student {
-	int gradeLevel;
-
-	public HighSchoolStudent(String name, int age, int gradeLevel) {
-		super(name, age);
-		this.gradeLevel = gradeLevel;
-	}
-
-	@Override
-	public void getDetails() {
-		System.out.println("High School Student:");
-		System.out.println("Name: " + getName());
-		System.out.println("Age: " + getAge());
-		System.out.println("Grade Level: " + gradeLevel);
-	}
-
-	@Override
-	public void displayDetails() {
-	System.out.println("Name: "+ name + "Age: "+ age );
-		
-	}
-}
-
-class CollegeStudent extends Student {
-	String major;
-
-	public CollegeStudent(String name, int age, String major) {
-		super(name, age);
-		this.major = major;
+		this.studentId = studentId;
 	}
 	@Override
-	public void getDetails() {
-		System.out.println("College Student:");
-		System.out.println("Name: " + getName());
-		System.out.println("Age: " + getAge());
-		System.out.println("Major: " + major);
+	public void study() {
+		System.out.println(name + " is studying for high school exams.");
 	}
 	@Override
-	public void displayDetails() {
-		System.out.println("Name: "+ name + "\nAge: "+ age );	
+	public void takeExam() {
+		System.out.println(name + " is taking high school exams.");
 	}
 }
 public class StudentDetails {
 	public static void main(String[] args) {
-		Student highSchoolStudent = new HighSchoolStudent("Harish", 22, 9);
-		Student collegeStudent = new CollegeStudent("Balaji", 20, "Computer Science");
-
-		highSchoolStudent.getDetails();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter name: ");
+		String name = scanner.nextLine();
+		System.out.println("Enter age: ");
+		int age = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("Enter student ID: ");
+		String studentId = scanner.nextLine();
+		HighSchoolStudent h1=new HighSchoolStudent(name, age, studentId);
+		h1.study();
+		h1.takeExam();
 		System.out.println();
-		collegeStudent.getDetails();
-		System.out.println();
-		System.out.println("StudentDetails: ");
-		collegeStudent.displayDetails();
-		
+		h1.displayInformation();
+		scanner.close();
 	}
 }
