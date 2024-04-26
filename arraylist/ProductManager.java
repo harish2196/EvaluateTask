8a07a9af157af26c6a3f75954dbab1f67b5bef90
Harish <144200799+harish2196@users.noreply.github.com>
2024-04-26 09:24:34 +0530

    Product Pricing App - ArrayList

--- arraylist/ProductManager.java
+++ arraylist/ProductManager.java
@@ -1,8 +1,9 @@
 package com.chainsys.arraylist;
-
+import com.chainsys.pojo.Validation;
+import com.chainsys.pricing.*;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import com.chainsys.pricing.*;
 public class ProductManager {
  ArrayList<Product> productList= new ArrayList<>();;
 
@@ -27,7 +28,77 @@ public class ProductManager {
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Validation v1=new Validation();	
         ProductManager productManager = new ProductManager();
+        String name,pass;
+		String passWord = "";
+		String userName = "";
+		System.out.println("Do you have any Account in our Store: \n(Yes or No)");
+		String choose=scanner.nextLine().toLowerCase();
+		while(!choose.equals("yes") && !choose.equals("no")) {
+			System.err.println("Please Re-enter Valid Input:");
+			choose=scanner.nextLine().toLowerCase();
+		}
+		if(choose.equals("no")) {
+			System.out.println("Please Sign Up");			
+			while (true) {
+				System.out.println("Enter UserName:");
+				userName = scanner.nextLine();
+				if (!v1.validateString(userName)) {
+					System.err.println("Invalid Username Name!");
+				} else {
+					break; 
+				}
+			}	
+			while (true) {
+				System.out.println("Enter PassWord:");
+				passWord= scanner.nextLine();
+				if (!v1.isSpecialChar(passWord)) {
+					System.err.println("Invalid PassWord Name!");
+				} else {
+					break; 
+				}
+			}
+			System.out.println("Signed Successfully!");
+			System.out.println("Please Log In!");
+			System.out.println("Enter The Name: ");
+			name=scanner.next();
+			while(!name.matches(userName)) {
+				System.err.println("Please Enter Valid UserName:");
+				name=scanner.next();
+			}
+			System.out.println("Enter The Password: ");
+			pass=scanner.next();
+			while(!pass.matches(passWord)) {
+				System.err.println("Please Enter Valid PassWord:");
+				pass=scanner.next();
+			}
+		} else if (choose.equals("yes")){
+			System.out.println("Please Log In!");
+			while (true) {
+				System.out.println("Enter UserName:");
+				name = scanner.nextLine();
+				if (!v1.validateString(name)) {
+					System.err.println("Invalid Username Name!");
+				} else {
+					break; 
+				}
+			}
+			while (true) {
+				System.out.println("Enter PassWord:");
+				pass= scanner.nextLine();
+				if (!v1.isSpecialChar(pass)) {
+					System.err.println("Invalid PassWord Name!");
+				} else {
+					break; 
+				}
+			}
+
+		}
+		System.out.println("Logg In Succesfully!");
+		System.out.println("Welcome to our store!");
+		System.out.println("What product would you like to get?");
+		
         System.out.println("Welcome to Product Manager");
         System.out.println("1. Add Product");
         System.out.println("2. View Products");
@@ -46,7 +117,7 @@ public class ProductManager {
                     int id = scanner.nextInt();
                     scanner.nextLine();
                     System.out.println("Enter Product Name:");
-                    String name = scanner.nextLine();
+                    name = scanner.nextLine();
                     System.out.println("Enter Product Model:");
                     String model = scanner.nextLine();
                     System.out.println("Enter Product Price:");
